686e9253e6de4148afa0359811fd6715622dc024
Swain9 <2786885366@qq.com>
2020-07-15 23:08:37 +0800

    对比js

---
+++ src/main/java/com/maolin/javascript/proto/S2Prototype.java
@@ -0,0 +1,9 @@
+package com.maolin.javascript.proto;
+
+/**
+ * @author zhangmaolin
+ * @date 2020-07-07 21:45
+ * @since 0.0.1
+ */
+public class S2Prototype extends S1Prototype{
+}
---
+++ src/test/java/com/maolin/javascript/PrototypeTest.java
@@ -0,0 +1,19 @@
+package com.maolin.javascript;
+
+import com.maolin.javascript.proto.S2Prototype;
+import com.maolin.javascript.proto.S3Prototype;
+import org.junit.Test;
+
+/**
+ * @author zhangmaolin
+ * @date 2020-07-07 21:45
+ * @since 0.0.1
+ */
+public class PrototypeTest {
+
+    @Test
+    public void test(){
+        S2Prototype.show();
+        S3Prototype.show();
+    }
+}
